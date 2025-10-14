be230558b80bfe298bdc8003b50235a687f917e6
Mawiyah13 <mawiyahh3@gmail.com>
2025-10-14 14:31:58 +0530

    updated

--- script.js
+++ script.js
@@ -149,3 +149,22 @@ console.log(obj3.name);
 let name = 'Mawiyah'
 let age = 21;
 console.log(`I'm ${name} and my age is ${age}`);
+
+//stack function
+const mul =(a,b)=>{
+    return a*b;
+}
+const square =(n)=>{
+    return mul(n,n);
+}
+const print = (n)=>{
+    let s = square(n);
+    console.log(s);
+}
+print(4);
+console.log('first');
+Promise.resolve().then(console.log('From promises'))
+setTimeout(()=>{
+    console.log('Inside Timeout');
+},2000)
+console.log('last')
