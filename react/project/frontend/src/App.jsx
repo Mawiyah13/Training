f55f7ada229970a548dea25d7bdd74300849497c
Mawiyah13 <mawiyahh3@gmail.com>
2025-10-15 16:30:53 +0530

    react files

---
+++ react/project/frontend/src/App.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import User from './components/User'
+import {Route,Routes} from "react-router-dom"
+import { BrowserRouter } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Service from './pages/Service'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <>
+        <Navbar/>
+        <Routes>
+          <Route path="/home"Component={Home}> </Route>
+          <Route path="/about"Component={About}></Route>
+          <Route path="/contact"Component={Contact}></Route>
+          <Route path="/service"Component={Service}></Route>
+        </Routes>
+        
+        
+        <User name="Mawiyah H" 
+        department="B.Tech IT" 
+        skill={["Java", "MERN"]} 
+        hobby={["Reading Books","Watching Movies"]} 
+        education={["Currently pursuing penultimate year of Bachelor of Information Technology", "Completed Higher Secondary Education"]}
+        />
+
+    </>
+  )
+}
+
+export default App
---
+++ react/project/frontend/src/components/User.jsx
@@ -0,0 +1,38 @@
+//rafce default arrow function
+//const User = (props) => {
+//    return (
+//        <div>
+//            <h1>I am {props.name} from {props.departmant}</h1>
+//        </div>
+//    )
+//}
+import React from 'react'
+
+const User = ({ name, department, skill, hobby,education}) => {
+  return (
+    <div>
+      <h1>Hi, I am {name}</h1>
+      <p>I am a {department} student from Sri Shakthi Institute of Engineering and Technology, aspiring to become a Full Stack Developer.</p>
+      <h3>SKILLS:</h3>
+      <ol>
+        {skill.map((item, id) => (
+          <li key={id}>{item}</li>
+        ))}
+      </ol>
+      <h3>HOBBIES:</h3>
+      <ol>
+        {hobby.map((item, id) => (
+          <li key={id}>{item}</li>
+        ))}
+      </ol>
+      <h3>EDUCATION:</h3>
+      <ol>
+        {education.map((item, id)=> (
+          <li key={id}>{item}</li>
+        ))}
+      </ol>
+    </div>
+  )
+}
+
+export default User
